67868c5862050d1a747c4ecadeb2d8a93ff002e3
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-09 12:32:01 -0400

    final commit

--- ASWTOLJSVariablesforNumbers.js
+++ ASWTOLJSVariablesforNumbers.js
@@ -9,3 +9,47 @@ Math
 // var shippingCharge = 10;
 // var orderTotal = merchTotal + shippingCharge;
 110
+// Form a statement that declares a variable and assigns a value to it.
+var niceBigNumber = 1000000;
+// In a single statement, declare the variable numberAsString and assign the number 3 to it as a string.
+var numberAsString = "3";
+// Assign to the variable caseQty, which has already been declared, the value 144.
+var caseQty;
+caseQty = 144;
+// In a single statement declare a variable and assign a number to it.
+var num = 1;
+// In a single statement declare a variable and assign to it the sum of 2 other variables. 
+var total = cost + expense;
+// Rewrite this statement so the variable can be used in a math operation.
+// var num = "9";
+var num = 9;
+// Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand.
+var sum = 1 + 1;
+// In one statement declare a variable. In a second statement assign it the sum of 2 numbers
+var total;
+total = 1 * 10;
+// In a single statement declare a variable and assign a number to it.
+var number = 10;
+// Time goal: 25 seconds. In a single statement declare a variable and assign to it the sum of 2 other variables. 
+var total 
+total = pay * hours;
+// Time goal: 20 seconds. Rewrite this statement so the variable can be used in a math operation.
+// var num = "9";
+var num = 9;
+// Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand.
+var cost = 10 + 12;
+// Time goal: 30 seconds. In one statement declare a variable. In a second statement assign it the sum of 2 numbers. 
+var timer;
+timer = 30 + 30;
+// Time goal: 35 seconds. In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.
+var orderCost = 1000;
+orderCost = orderCost + 10;
+// Declare a variable and assign it a number. Then code an alert, specifying the variable, not the number, as the message. Click the Run It button to run your code live.
+var numAlert = 10;
+alert(numAlert);
+// Declare a variable and assign it a number.
+// Double the variable's value by adding the variable to itself.
+// Display the variable's value in an alert, specifying the variable, not the number, as the message.
+var finalCost = 100;
+finalCost = finalCost * 2;
+alert(finalCost);
