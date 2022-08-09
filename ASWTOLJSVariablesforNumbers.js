73add4a0744f84bef846e5d1b90a5691f26260fa
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-09 11:39:17 -0400

    added awnsers

---
+++ ASWTOLJSVariablesforNumbers.js
@@ -0,0 +1,11 @@
+// You can assign a string to a variable. You can also assign a ______.
+number
+// If a number is enclosed in quotes, it's a ______.
+String
+// What kind of operation can be done on a number variable that, in one particular case, can't be done on a string variable? Answer with a 4-letter word. (It would be a five-letter word in British usage.)
+Math
+// What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+110
